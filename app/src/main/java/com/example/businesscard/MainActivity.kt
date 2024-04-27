1a56dab210eeddb25ec0d50464a8b4287c26d25e
gustavorods <gustavo.rlsilva07@gmail.com>
2024-04-27 08:55:36 -0300

    feat: Introducion Cards done

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +30,7 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    middleElements("Gustavo", "FullStack Developer")
+
                 }
             }
         }
@@ -35,36 +38,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun middleElements(name: String, subTitle: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.ic_launcher_background)
-   Box(modifier = Modifier) {
-       Column(
-           modifier = Modifier
-           //.align(Alignment.CenterHorizontally)
-           .padding(horizontal = 16.dp),
-           verticalArrangement = Arrangement.Center
-       ) {
-           Image(
-               painter = image,
-               contentDescription = null,
-           )
-           Text(
-               text = name,
-               lineHeight = 116.sp,
-               modifier = modifier
-           )
-           Text(
-               text = subTitle,
-               modifier = modifier
-           )
-       }
-   }
+fun IntroductionCard() {
+    var photo = painterResource(R.drawable.ic_launcher_background)
+    Column(Modifier
+        .fillMaxWidth(),
+        Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth(),
+                Arrangement.Center,
+        ) {
+            Image(painter = photo, contentDescription = null)
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(text = "Gustavo Rodrigues",
+                fontSize = 25.sp
+                )
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(text = "Back-end developer",
+                fontSize = 15.sp)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    BusinessCardTheme {
-        middleElements("Gustavo", "FullStack Developer")
+@Preview
+fun cardPreview() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        IntroductionCard()
     }
 }
